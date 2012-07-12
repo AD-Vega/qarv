5dd8f0692c28250e5d4b11be73d24971eece6fe7
Jure Varlec <jure.varlec@ad-vega.si>
2012-07-12 09:11:00 +0200

    Add decoders for 12bit formats. (UNTESTED)

--- framedecoders.cpp
+++ framedecoders.cpp
@@ -19,6 +19,7 @@
 #include "framedecoders.h"
 #include <QHash>
 #include <cstdlib>
+#include <cassert>
 
 static QVector<QRgb> initGraymap() {
   QVector<QRgb> map(256);
@@ -35,7 +36,7 @@ public:
   QImage decode(QByteArray frame) {
     QImage img(size, QImage::Format_Indexed8);
     img.setColorTable(graymap);
-    char* dta = frame.data();
+    const char* dta = frame.constData();
     const int h = size.height(), w = size.width();
     for (int i=0; i<h; i++)
       memcpy(img.scanLine(i), dta+i*w, w);
@@ -45,6 +46,69 @@ public:
 private:
   QSize size;
 };
+
+class Mono12: public FrameDecoder {
+public:
+  Mono12(QSize size_): size(size_) {};
+  ~Mono12() {}
+  QImage decode(QByteArray frame) {
+    QImage img(size, QImage::Format_Indexed8);
+    img.setColorTable(graymap);
+    const uchar* dta = reinterpret_cast<const uchar*>(frame.constData());
+    const int h = size.height(), w = size.width(), W = img.bytesPerLine();
+    auto I = img.bits();
+    for (int i=0; i<h; i++)
+      for (int j=0; j<w; j++)
+        I[i*W+j] = dta[i*w+j] >> 4;
+  }
+private:
+  QSize size;
+};
+
+class Mono12Packed: public FrameDecoder {
+public:
+  Mono12Packed(QSize size_): size(size_) {};
+  ~Mono12Packed() {}
+  QImage decode(QByteArray frame) {
+    QImage img(size, QImage::Format_Indexed8);
+    img.setColorTable(graymap);
+    const uchar* dta = reinterpret_cast<const uchar*>(frame.constData());
+    const int h = size.height(), w = size.width(), W = img.bytesPerLine();
+    assert(frame.size() % 2 == 0);
+    assert(h*w % 2 == 0);
+
+    int line = 0;
+    uchar* outptr = img.scanLine(0);
+    uchar* outptr_base = outptr;
+    const uchar* inptr = dta;
+    uint16_t pixel;
+    char * bytes = reinterpret_cast<char*>(&pixel);
+    while (inptr < dta + frame.size()) {
+      bytes[0] = inptr[1] << 4;
+      bytes[1] = inptr[0];
+      pixel >>= 8;
+      *(outptr++) = pixel;
+
+      if (outptr - outptr_base == w) {
+        if (line++ == h) break;
+        outptr = outptr_base = img.scanLine(line);
+      }
+
+      bytes[0] = inptr[1] << 4;
+      bytes[1] = inptr[2];
+      pixel >>= 8;
+      *(outptr++) = pixel;
+
+      if (outptr - outptr_base == w) {
+        if (line++ == h) break;
+        outptr = outptr_base = img.scanLine(line);
+      }
+    }
+  }
+private:
+  QSize size;
+};
+
 
 typedef FrameDecoder* (*makeDecoderFcn) (QSize);
 
@@ -56,6 +120,8 @@ FrameDecoder* makeDecoder(QSize size) {
 static QHash<QString, makeDecoderFcn> listalldecoders() {
   QHash<QString, makeDecoderFcn> list;
   list.insert("Mono8", makeDecoder<Mono8>);
+  list.insert("Mono12", makeDecoder<Mono12>);
+  list.insert("Mono12Packed", makeDecoder<Mono12Packed>);
   return list;
 }
 
