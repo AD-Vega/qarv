580ce65d87a04a9ceb10a7ba6ba432ce880aa364
Jure Varlec <jure.varlec@ad-vega.si>
2013-04-30 15:14:33 +0200

    Port swscaledecoder to use CV2QImage.

--- src/decoders/swscaledecoder.cpp
+++ src/decoders/swscaledecoder.cpp
@@ -27,12 +27,6 @@ extern "C" {
 
 using namespace QArv;
 
-#if Q_BYTE_ORDER == Q_BIG_ENDIAN
-#define BUFFER_PIXFMT PIX_FMT_RGB48BE
-#else
-#define BUFFER_PIXFMT PIX_FMT_RGB48LE
-#endif
-
 SwScaleDecoder::SwScaleDecoder(QSize size_, PixelFormat inputPixfmt_, ArvPixelFormat arvPixFmt) :
   size(size_), image_pointers { 0, 0, 0 }, image_strides { 0, 0, 0 },
   inputPixfmt(inputPixfmt_), arvPixelFormat(arvPixFmt) {
@@ -48,7 +42,7 @@ SwScaleDecoder::SwScaleDecoder(QSize size_, PixelFormat inputPixfmt_, ArvPixelFo
     buffer = new uint16_t[size.width()*size.height()*3];
     image_pointers[0] = reinterpret_cast<uint8_t*>(buffer);
     ctx = sws_getContext(size.width(), size.height(), inputPixfmt,
-                         size.width(), size.height(), BUFFER_PIXFMT,
+                         size.width(), size.height(), PIX_FMT_BGR48,
                          SWS_BICUBIC, 0, 0, 0);
   } else {
     qDebug() << "Pixel format" << av_get_pix_fmt_name(inputPixfmt)
@@ -89,29 +83,12 @@ void SwScaleDecoder::decode(QByteArray frame) {
 
 QImage SwScaleDecoder::getQImage() {
   if (!OK) return QImage();
-  QImage out(size, QImage::Format_RGB888);
-  int width = size.width(), height = size.height();
-  for (int i = 0; i < height; i++) {
-    auto l = out.scanLine(i);
-    for (int j = 0; j < width; j++) {
-      for (int px = 0; px < 3; px++) {
-        l[3*j + px] = buffer[3*(i*width + j) + px] >> 8;
-      }
-    }
-  }
-  return out;
+  cv::Mat M(size.height(), size.width(), CV_16UC3, buffer);
+  return QArvDecoder::CV2QImage(M);
 }
 
 cv::Mat SwScaleDecoder::getCvImage() {
   if (!OK) return cv::Mat();
-  cv::Mat M(size.height(), size.width(), CV_16UC3);
-  for(int i = 0; i < M.rows; i++) {
-    auto Mr = M.ptr<cv::Vec<uint16_t, 3>>(i);
-    for (int j = 0; j < M.cols; j++) {
-      auto& bgr = Mr[j];
-      for (int px = 0; px < 3; px++)
-        bgr[px] = buffer[3*(i*M.cols + j) + (2 - px)];
-    }
-  }
-  return M;
+  cv::Mat M(size.height(), size.width(), CV_16UC3, buffer);
+  return M.clone();
 }
